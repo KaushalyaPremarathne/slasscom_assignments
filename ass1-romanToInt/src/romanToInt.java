756910b677d87d187cb5281314506017b078c91c
KaushalyaPremarathne <it20633622@my.sliit.lk>
2022-12-07 10:04:52 +0000

    initial commit - store string in a char array

---
+++ ass1-romanToInt/src/romanToInt.java
@@ -0,0 +1,14 @@
+
+public class romanToInt {
+
+	public static void main(String[] args) {
+		
+		String s = "XX";
+		
+		char arr[] = s.toCharArray();
+		
+		for(int i = 0; i < s.length(); i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
