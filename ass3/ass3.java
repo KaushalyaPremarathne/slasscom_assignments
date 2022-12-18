7c25127cb42df99e177f9952a75c1e20d96e24f1
KaushalyaPremarathne <it20633622@my.sliit.lk>
2022-12-18 21:26:47 +0000

    find prime numbers

--- ass3/ass3.java
+++ ass3/ass3.java
@@ -4,8 +4,7 @@ public class ass3 {
 
     public static void selectionSort(int[] numArray){
 
-
-
+        //Sorting the array using selection sort
         int size = numArray.length;
 
         for (int i = 0; i < size - 1; i++)
@@ -29,19 +28,23 @@ public class ass3 {
         int size = numArray.length;
 
         selectionSort(numArray);//sorting array using selection sort
-        
+
         int maxCount = 0;
         int median = 0;
         int mode = 0;
 
+        //calculate median
         if(size % 2 == 0)
             median = (numArray[size/2] + numArray[(size/2)+1]) / 2;
         else
             median = numArray[size/2];
 
         System.out.println("Median : " + median);
+
+        //calculate highest value
         System.out.println("Highest Value : " + numArray[size-1]);
 
+        //calculate mode
         for(int i = 0; i < size; i++){
             int count = 0;
             for(int j = 0; j < size; j++){
@@ -56,5 +59,24 @@ public class ass3 {
         }
 
         System.out.println("Mode : " + mode);
+
+        System.out.println();
+        
+        //print prime numbers
+        System.out.println("Prime Numbers");
+        for( int m = 0; m < size; m++){
+            int correct = 0;
+            int incorrect = 0;
+
+            for( int n = 1; n < numArray[m]; n++){
+                if(numArray[m] % n == 0)
+                    correct++;
+                else
+                    incorrect++;
+            }
+
+            if(correct == 1)
+                System.out.println(numArray[m]);
+        }
     }
 }
