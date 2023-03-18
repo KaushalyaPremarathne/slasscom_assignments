bfac0be132df4cb797d10100632c64c1bbacf3b6
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-03-18 16:18:36 +0000

    user interface implementation

--- finalAssignment/Q3/ChatBot.java
+++ finalAssignment/Q3/ChatBot.java
@@ -7,9 +7,20 @@ import java.util.concurrent.ExecutionException;
 import ai.openai.gpt.Gpt;
 import ai.openai.gpt.GptCompletionResponse;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class Chatbot {
 
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "sk-Gsv94h3B2xBS39FwOBIhT3BlbkFJIEdM9OoukkWzQVhujhi";
     private static final String MODEL_NAME = "davinci";
     private static final int MAX_TOKENS = 50;
     private static final double TEMPERATURE = 0.5;
@@ -26,26 +37,48 @@ public class Chatbot {
     }
 
     public void start() {
-        System.out.println("Welcome to our online sales store chatbot! How can I assist you today?");
-        while (true) {
-            String userInput = scanner.nextLine().trim();
-            chatHistory.addCustomerMessage(userInput);
-            if (userInput.equalsIgnoreCase("exit")) {
-                break;
-            }
-            CompletableFuture<GptCompletionResponse> future = gpt.complete(userInput, MAX_TOKENS, TEMPERATURE);
-            GptCompletionResponse response = null;
-            try {
-                response = future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-            if (response != null) {
-                String chatbotResponse = response.getText().trim();
-                System.out.println(chatbotResponse);
-                chatHistory.addChatbotMessage(chatbotResponse);
-            }
-        }
+    	
+    	 BorderPane root = new BorderPane();
+         chatArea = new TextArea();
+         chatArea.setEditable(false);
+         chatArea.setWrapText(true);
+         inputArea = new TextArea();
+         inputArea.setPromptText("Type your message here");
+         inputArea.setWrapText(true);
+         Button sendButton = new Button("Send");
+         
+         sendButton.setOnAction(e -> {
+        	 System.out.println("Welcome to our online sales store chatbot! How can I assist you today?");
+             while (true) {
+                 String userInput = scanner.nextLine().trim();
+                 chatHistory.addCustomerMessage(userInput);
+                 if (userInput.equalsIgnoreCase("exit")) {
+                     break;
+                 }
+                 CompletableFuture<GptCompletionResponse> future = gpt.complete(userInput, MAX_TOKENS, TEMPERATURE);
+                 GptCompletionResponse response = null;
+                 try {
+                     response = future.get();
+                 } catch (InterruptedException | ExecutionException e) {
+                     e.printStackTrace();
+                 }
+                 if (response != null) {
+                     String chatbotResponse = response.getText().trim();
+                     System.out.println(chatbotResponse);
+                     chatHistory.addChatbotMessage(chatbotResponse);
+                 }
+             }
+         });
+         
+         HBox inputBox = new HBox(10, inputArea, sendButton);
+         VBox chatBox = new VBox(10, new Label("Chat"), chatArea, inputBox);
+         chatBox.setPadding(new Insets(10));
+         root.setCenter(chatBox);
+
+         Scene scene = new Scene(root, 600, 400);
+         primaryStage.setScene(scene);
+         primaryStage.setTitle("Online Sales Store Chatbot");
+         primaryStage.show();
     }
     
     private static class ChatHistory {
